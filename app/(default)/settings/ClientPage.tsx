13b1067da67f3a6fa2eaf930faa93e08ffad55d5
Suhyun Park <me@shiftpsh.com>
2024-12-06 05:17:16 +0900

    fix: logout menu

--- app/(default)/settings/ClientPage.tsx
+++ app/(default)/settings/ClientPage.tsx
@@ -3,17 +3,8 @@ import { List, ListItem, ListItemButton } from "@mui/joy";
 const ClientPage = () => {
   return (
     <List>
-      <ListItem>
-        <ListItemButton>설정 1</ListItemButton>
-      </ListItem>
-      <ListItem>
-        <ListItemButton>설정 2</ListItemButton>
-      </ListItem>
-      <ListItem>
-        <ListItemButton>설정 3</ListItemButton>
-      </ListItem>
-      <ListItem>
-        <ListItemButton>설정 4</ListItemButton>
+      <ListItem component="a" href="/api/auth/logout/">
+        <ListItemButton>로그아웃</ListItemButton>
       </ListItem>
     </List>
   );
